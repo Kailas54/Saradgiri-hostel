a84dcdb0909fe7208df5eb34784cf37dbf5a4aaa
Kailas54 <kidg248@gmail.com>
2026-07-01 22:48:54 +0530

    Revert react-snap to fix Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2015'
-  },
   server: {
     port: 3000,
     strictPort: true,
